64201c1c8363f37bc9051bad8251298b495609e2
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-12 18:22:39 -0800

    Implement firstLine and statusCode methods in callback review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,17 +6,33 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  var stream = fs.createReadStream(filePath);
+  var firstLine = '';
+  stream.on('data', chunk => {
+    firstLine += chunk;
+  }).on('close', () => {
+    firstLine = firstLine.slice(0, firstLine.indexOf('\n'));
+    callback(null, firstLine);
+  }).on('error', err => {
+    callback(err);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, (err, res) => {
+    if (err) {
+      callback(err);
+    }
+    if (res) {
+      callback(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
-  getStatusCode: getStatusCode,
-  pluckFirstLineFromFile: pluckFirstLineFromFile
+  getStatusCode,
+  pluckFirstLineFromFile
 };
